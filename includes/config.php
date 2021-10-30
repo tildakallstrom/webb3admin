5d0642134ba53b7c660e45eb75fc28f5e0275938
Tilda Källström <66213537+tildakallstrom@users.noreply.github.com>
2021-10-30 20:04:40 +0200

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,29 +5,12 @@ spl_autoload_register(function ($class_name) {
     include 'classes/' . $class_name . '.class.php';
 });
 
-/*
-$host = "tildakallstrom.se.mysql";
-$user = "tildakallstrom_seadmin";
-$pass = "adminadmin";
-$dbb = "tildakallstrom_seadmin";
-$conn = mysqli_connect($host, $user, $pass, $dbb); */
 
-// Db settings remote
-/*define("DBHOST", "tildakallstrom.se.mysql");
-define("DBUSER", "tildakallstrom_seadmin");
-define("DBPASS", "adminadmin");
-define("DBDATABASE", "tildakallstrom_seadmin");
-*/
-/*
-define("DBHOST", "localhost");
-define("DBUSER", "admin");
-define("DBPASS", "adminadmin");
-define("DBDATABASE", "admin");
-*/
-define("DBHOST", "studentmysql.miun.se");
-define("DBUSER", "tika1900");
-define("DBPASS", "y8rwy9QKTW");
-define("DBDATABASE", "tika1900");
+
+define("DBHOST", "x");
+define("DBUSER", "x");
+define("DBPASS", "x");
+define("DBDATABASE", "x");
 
 // Start session
 if(session_status() == PHP_SESSION_NONE) {
